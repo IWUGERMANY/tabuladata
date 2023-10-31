cf4d3fcb5be25d8bb378cf551c7724518d3bfbb5
Jens C <179465+lateshift@users.noreply.github.com>
2023-10-31 20:16:48 +0100

    Initial import

---
+++ R/data.R
@@ -0,0 +1,358 @@
+#' AdditionalPar
+#'
+#' National addtitional parameter for classification (regions,
+#' special building types etc.)
+"additionalpar"
+
+#' AtticCond
+#'
+#' Space heating situation of the attic storey
+"atticcond"
+
+#' AttNeighb
+#'
+#' Codes for the number of attached neighbour buildings
+"attneighb"
+
+#' BoundaryCond
+#'
+#' Boundary conditions for the energy balance calculation (optional)
+"boundarycond"
+
+#' BoundaryCondType
+#'
+#' Type of boundary condition input
+"boundarycondtype"
+
+#' Building.Constr
+#'
+#' National definition of construction elements + U-values
+"building.constr"
+
+#' Building.Measure
+#'
+#' National definition of insulation measures + thermal resistance
+"building.measure"
+
+#' BuildingSizeClass
+#'
+#' Building size classes
+"buildingsizeclass"
+
+#' BuildingSizeClass
+#'
+#' Building size classes
+"buildingsizeclass"
+
+#' CalcAdapt
+#'
+#' Definition of calibration factors for adapting the TABULA calculation
+#' results to the typical level of metered consumption or to the typical
+#' level of national calculation methods (used for respective estimations)
+"calcadapt"
+
+#' CellarCond
+#'
+#' Space heating situation of the cellar storey
+"cellarcond"
+
+#' Climate
+#'
+#' National and regional climate conditions
+"climate"
+
+#' ComplexFootprint
+#'
+#' Degree of geometrical complexity of the footprint shape
+"complexfootprint"
+
+#' ComplexRoof
+#'
+#' Degree of geometrical complexity of the roof shape
+"complexroof"
+
+#' ConstrBorder
+#'
+#' Type of construction border situation / location of construction element
+"constrborder"
+
+#' ConstrYearClass
+#'
+#' National construction year classes
+"constryearclass"
+
+#' Country
+#'
+#' Country codes
+"country"
+
+#' DataType.Building
+#'
+#' Building data type
+"datatype.building"
+
+#' ElementType
+#'
+#' Thermal envelope element types
+"elementtype"
+
+#' EnergyCarrier
+#'
+#' Codes for the used energy carriers
+"energycarrier"
+
+#' Infiltration
+#'
+#' Classification of infiltration, dependent on air tightness
+"infiltration"
+
+#' Insulation.Default
+#'
+#' National default values of insulation thickness and thermal
+#' conductivity of insulation measures, differentiated by
+#' implementation period and construction type;
+#' to be used if the actual values are not known for a given
+#' construction (usage of these default values increases the uncertainty)
+"insulation.default"
+
+#' Language
+#'
+#' Language codes
+"language"
+
+#' Measure.f.Default
+#'
+#' National default values of insulation fraction, differentiated
+#' by construction type, implementation period; two cases:
+#' (a) typical insulation fraction of refurbished construtions
+#' (indicator "Refurbished", used if actual insulation fraction
+#' is not known for a construction)
+#'
+#' (b) ratio of insulated to total construction area for the building
+#' stock of the specific construction period (indicator "NA",
+#' values used to model an average state for constructions of
+#' unknown state) - usage of these default values increases
+#' the uncertainty
+"measure.f.default"
+
+#' MeasureType
+#'
+#' Type of refurbishment measure / replacement of existing
+#' insulation or elements
+"measuretype"
+
+#' TODO
+#'
+#' yyyy
+"meter.energydensity"
+
+#' Par.EnvAreaEstim
+#'
+#' Parameters for the envelope area estimation procedure
+"par.envareaestim"
+
+#' RoofType
+#'
+#' Roof type codes
+"rooftype"
+
+#' StatusDataset
+#'
+#' Dataset status codes
+"statusdataset"
+
+#' System.Coverage
+#'
+#' Default values of coverages depending on supply/load ratio
+"system.coverage"
+
+#' System.EC
+#'
+#' Energy carrier specification and assessment
+"system.ec"
+
+#' System.ElProd
+#'
+#' Assessment of on-site produced electricity
+"system.elprod"
+
+#' System.H
+#'
+#' National configurations of heating system
+"system.h"
+
+#' System.HA
+#'
+#' Heating system / auxiliary energy default values
+"system.ha"
+
+#' System.HD
+#'
+#' Heating system / heat distribution default values
+"system.hd"
+
+#' System.HG
+#'
+#' Heating system / heat generation default values
+"system.hg"
+
+#' System.HS
+#'
+#' Heating system / heat storage default values
+"system.hs"
+
+#' System.PV
+#'
+#' Default values of PV electricity production depending on orientation
+"system.pv"
+
+#' System.PVPanel
+#'
+#' Default values of PV electricity production depending on orientation
+"system.pvpanel"
+
+#' System.SetECAssess
+#'
+#' Definition of energy carrier assessment sets
+"system.setecassess"
+
+#' System.Vent
+#'
+#' Default values of ventilation systems
+"system.vent"
+
+#' System.Vent
+#'
+#' Default values of ventilation systems
+"system.vent"
+
+#' System.W
+#'
+#' National configurations of domestic hot water systems
+"system.w"
+
+#' System.WA
+#'
+#' Domestic hot water system / auxiliary energy default values
+"system.wa"
+
+#' System.WD
+#'
+#' Domestic hot water system / heat distribution default values
+"system.wd"
+
+#' System.WG
+#'
+#' Domestic hot water system / heat generation default values
+"system.wg"
+
+#' System.WS
+#'
+#' Domestic hot water system / heat storage default values
+"system.ws"
+
+#' System.HA
+#'
+#' Heating system / auxiliary energy default values
+"system.ha"
+
+#' System.HD
+#'
+#' Heating system / heat distribution default values
+"system.hd"
+
+#' System.HG
+#'
+#' Heating system / heat generation default values
+"system.hg"
+
+#' System.HS
+#'
+#' Heating system / heat storage default values
+"system.hs"
+
+#' SysType.Size
+#'
+#' Building types used to distinguish between different system component sizes
+"systype.size"
+
+#' SysType.Vent
+#'
+#' Heating system: ventilation types
+"systype.vent"
+
+#' SysType.WA
+#'
+#' Domestic hot water system: auxiliary energy types
+"systype.wa"
+
+#' SysType.WD
+#'
+#' Domestic hot water system: distribution types
+"systype.wd"
+
+#' SysType.WG
+#'
+#' Domestic hot water system: generator types
+"systype.wg"
+
+#' SysType.WS
+#'
+#' Domestic hot water system: storage types
+"systype.ws"
+
+#' ThermalBridging
+#'
+#' Classification of thermal bridging
+"thermalbridging"
+
+#' Type.CalcAdapt
+#'
+#' Calculation adaptation types
+"type.calcadapt"
+
+#' TypeIntake
+#'
+#' Types of data intake
+"typeintake"
+
+#' TypeVariant
+#'
+#' Type of building variant (refurbishment variant or new build variant)
+"typevariant"
+
+#' TypologyRegion
+#'
+#' National typology regions
+"typologyregion"
+
+#' U.Class.Constr
+#'
+#' National default U-values differentiated by construction
+#' year class and construction type
+"u.class.constr"
+
+#' U.Class.Window
+#'
+#' U-values of windows (old scheme used for TABULA WebTool
+#' (only three time bands))
+"u.class.window"
+
+#' U.WindowType.Periods
+#'
+#' National default U-values of windows, diferentiated by
+#' construction year class and type of glazing and frame
+"u.windowtype.periods"
+
+#' Uncertainty.Levels
+#'
+#' Uncertainty levels: 5 categories of input data uncertainty;
+#' one row for each relevant input variable; for each relevant
+#' input variable: explanation of each category;
+#' quantification of absolute and/or relative uncertainty
+"uncertainty.levels"
+
+#' Utilisation
+#'
+#' Common definitions of utilisation types
+"utilisation"
